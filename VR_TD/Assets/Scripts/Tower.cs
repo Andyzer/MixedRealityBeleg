bf7508d5ec83e2fc62f34ccc985d4ea2cfb962ee
Andreas Marton <77020371+Andyzer@users.noreply.github.com>
2024-01-26 10:43:26 +0100

    new mats

---
+++ VR_TD/Assets/Scripts/Tower.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+
+    public float range = 3f;
+
+    void FindNextTarget()
+    {
+        
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
