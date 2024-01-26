50eb617a7f51c60810aa9348ae073a44816611aa
Andreas Marton <77020371+Andyzer@users.noreply.github.com>
2024-01-26 11:55:35 +0100

    i dont even know anymore

--- VR_TD/Assets/Scripts/Tower.cs
+++ VR_TD/Assets/Scripts/Tower.cs
@@ -1,26 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Properties;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-
-    public float range = 3f;
-
-    void FindNextTarget()
-    {
-        ////RANDOM SHIT FOR GITHUB
-    }
+    public Transform target;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float range = 3.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        FindNextTarget();
+        if (target != null)
+        {
+            AimAtTarget();
+        }
     }
+    
+    void FindNextTarget()
+    {
+        Collider[] enemies = Physics.OverlapSphere(transform.position, range, 1 << 8); // Adjust layer mask as per your setup
+
+        if (enemies.Length > 0)
+        {
+            float shortestDistance = Mathf.Infinity;
+            foreach (Collider enemy in enemies)
+            {
+                float distance = Vector3.Distance(transform.position, enemy.transform.position);
+                if (distance < shortestDistance)
+                {
+                    shortestDistance = distance;
+                    target = enemy.transform;
+                }
+            }
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    void AimAtTarget()
+    {
+        Vector3 lookPosition = target.position - transform.position;
+        lookPosition.y = 0;
+
+        Quaternion rotation = Quaternion.LookRotation(lookPosition);
+        transform.rotation = rotation;
+    }
+
+
+    
 }
