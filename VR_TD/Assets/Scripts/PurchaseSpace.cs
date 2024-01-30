a75e58b753b5956887cb020f2edcf8410740cb63
Andreas Marton <77020371+Andyzer@users.noreply.github.com>
2024-01-30 12:37:44 +0100

    further scripting, unity bullshit

--- VR_TD/Assets/Scripts/PurchaseSpace.cs
+++ VR_TD/Assets/Scripts/PurchaseSpace.cs
@@ -8,8 +8,26 @@ public class PurchaseSpace : MonoBehaviour
 {
     public TMP_Text moneyText;
 
+    public GameObject simpleTowerPrefab;
+    GameObject boughtTower;
+
     void Update()
     {
         moneyText.SetText("Money $" + Money.Amount);
+    }
+
+    public void BuySimpleTower()
+    {
+        //enough money? Tower already bought?
+        if (Money.Amount < 40 || boughtTower != null)
+            return;
+        
+        //Spawn new Tower
+        boughtTower = Instantiate(simpleTowerPrefab, Camera.main.ScreenToWorldPoint(new Vector3(0.5f, 0.5f)), Quaternion.identity);
+
+        //Make tower transparent
+        boughtTower.GetComponentInChildren<MeshRenderer>().material.color = new Color(1,1,1,0.5f);
+        
+        
     }
 }
