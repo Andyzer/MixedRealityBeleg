0e1d84d0f839b11727a622dafd1127bc3d0153f4
Andreas Marton <77020371+Andyzer@users.noreply.github.com>
2024-02-01 18:54:32 +0100

    fuck if i know anymore

--- VR_TD/Assets/Scripts/PurchaseSpace.cs
+++ VR_TD/Assets/Scripts/PurchaseSpace.cs
@@ -8,26 +8,73 @@ public class PurchaseSpace : MonoBehaviour
 {
     public TMP_Text moneyText;
 
-    public GameObject simpleTowerPrefab;
+    public GameObject towerPrefab;
     GameObject boughtTower;
 
     void Update()
     {
         moneyText.SetText("Money $" + Money.Amount);
+        
+        if (boughtTower != null)
+            MovePurchasedTower();
     }
 
-    public void BuySimpleTower()
+    void MovePurchasedTower()
+    {
+        boughtTower.transform.position = Camera.main.transform.position +
+                                         Camera.main.transform.forward * 5;
+    }
+
+    void CheckForWall()
+    {
+        Ray raycast = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+        
+        //TODO Remove next line
+        Debug.DrawRay(raycast.origin, raycast.direction * 100, Color.red);
+
+        if (Physics.Raycast(raycast, out hit))
+        {
+            if (hit.collider.gameObject.tag == "Wall")
+            {
+                //Put tower on wall
+                boughtTower.transform.position = hit.collider.gameObject.transform.position +
+                                                 new Vector3(0, 0.75f, 0);
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //take away wall tag
+                    hit.collider.gameObject.tag = "Untagged";
+                
+                    Color color = boughtTower.GetComponent<MeshRenderer>().material.color;
+                    color.a = 1f;
+                    boughtTower.GetComponent<MeshRenderer>().material.color = color;
+                
+                    //enable tower script
+                    boughtTower.GetComponent<Tower>().enabled = true;
+                    boughtTower = null;    
+                }
+                
+            }
+        }
+    }
+    
+    public void BuyTower()
     {
         //enough money? Tower already bought?
         if (Money.Amount < 40 || boughtTower != null)
             return;
         
         //Spawn new Tower
-        boughtTower = Instantiate(simpleTowerPrefab, Camera.main.ScreenToWorldPoint(new Vector3(0.5f, 0.5f)), Quaternion.identity);
+        boughtTower = Instantiate(towerPrefab, Camera.main.ScreenToWorldPoint(new Vector3(0.5f, 0.5f)), Quaternion.identity);
 
         //Make tower transparent
-        boughtTower.GetComponentInChildren<MeshRenderer>().material.color = new Color(1,1,1,0.5f);
         
-        
+        Color color = boughtTower.GetComponent<MeshRenderer>().material.color;
+       color.a = 0.5f;
+       boughtTower.GetComponent<MeshRenderer>().material.color = color;
+       
+       //Take money away
+       Money.Amount -= 40;
     }
 }
